5f35e2641bab67e088b03f3bff07b74e677605e3
Daniel Perryman <daniel.perryman@tcgplayer.com>
2022-12-09 13:27:09 -0500

    checking in day 9

--- PuzzleRunner/Day09Runner.cs
+++ PuzzleRunner/Day09Runner.cs
@@ -4,8 +4,8 @@ namespace PuzzleRunners
 {
     public class Day09Runner : BaseRunner<Day09>
     {
-        protected override string Puzzle1Solution => "TODO";
+        protected override string Puzzle1Solution => "6236";
 
-        protected override string Puzzle2Solution => "TODO";
+        protected override string Puzzle2Solution => "2449";
     }
 }
--- Puzzles/Solutions/Day09.cs
+++ Puzzles/Solutions/Day09.cs
@@ -4,14 +4,210 @@
     {
         public int Day => 9;
 
+        public class Knot 
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+        }
+
+        public class Grid
+        {
+            List<List<bool>> Positions { get; set; }
+
+            private List<Knot> Knots { get; set; }
+
+            private int MaxY { get; set; }
+
+            public Grid(int knotsCount)
+            {
+                Positions = new List<List<bool>>();
+                Positions.Add(new List<bool> { true });
+
+                Knots = new List<Knot>();
+
+                while (knotsCount > 0)
+                {
+                    Knots.Add(new Knot { X = 0, Y=0 }); 
+                    knotsCount--;
+                }
+
+            }
+
+            public void Move(char direction, int moves)
+            {
+                while (moves > 0)
+                {
+                    switch (direction)
+                    {
+                        case 'R':
+                            Knots[0].X++;
+                            break;
+                        case 'L':
+                            Knots[0].X--;
+                            break;
+                        case 'U':
+                            Knots[0].Y++;
+                            break;
+                        case 'D':
+                            Knots[0].Y--;
+                            break;
+                    }
+                    MaxY = Knots[0].Y > MaxY ? Knots[0].Y : MaxY;
+                    if (Knots[0].X >= Positions.Count || Knots[0].X < 0)
+                    {
+                        AddRows();
+                    }
+                    if (Knots[0].Y < 0)
+                    {
+                        AddColumn();
+                    }
+                    FillRows();
+                    DetermineMoves();
+                    moves--;
+                }
+            }
+
+            public void DetermineMoves()
+            {
+                for (int x = 0; x < Knots.Count() - 1; x++)
+                {
+                    var lead = Knots[x];
+                    var follow = Knots[x + 1];
+
+                    if (lead.X == follow.X)
+                    {
+                        if (lead.Y == follow.Y)
+                        {
+                            //Same Spot
+                            return;
+                        }
+                        else
+                        {
+                            //Vertical Move
+                            if (Math.Abs(lead.Y - follow.Y) > 1)
+                            {
+                                MoveY(lead, follow);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (lead.Y == follow.Y)
+                        {
+                            //Horizontal Move
+                            if (Math.Abs(lead.X - follow.X) > 1)
+                            {
+                                MoveX(lead, follow);
+                            }
+                        }
+                        else
+                        {
+                            if (Math.Abs(lead.Y - follow.Y) > 1 || Math.Abs(lead.X - follow.X) > 1)
+                            {
+                                //Diagonal Move
+                                MoveX(lead, follow);
+                                MoveY(lead, follow);
+                            }
+                        }
+                    }
+                }
+
+                var tail = Knots[Knots.Count - 1];
+                Positions[tail.X][tail.Y] = true;
+            }
+
+            public void AddRows()
+            {
+                if (Knots[0].X >= 0)
+                {
+                    for (int x = Knots[0].X - Positions.Count() + 1; x > 0; x--)
+                    {
+                        Positions.Add(new List<bool>());
+                    }
+                }
+                else
+                {
+                    var newGrid = new List<List<bool>>();
+                    newGrid.Add(new List<bool>());
+                    newGrid.AddRange(Positions);
+                    Positions = newGrid;
+                    foreach (var knot in Knots)
+                    {
+                        knot.X++;
+                    }
+                }
+            }
+
+            public void AddColumn()
+            {
+                for (int x = 0; x < Positions.Count(); x++)
+                {
+                    var newRow = new List<bool>();
+                    newRow.Add(false);
+                    newRow.AddRange(Positions[x]);
+                    Positions[x] = newRow;
+                }
+                foreach (var knot in Knots)
+                {
+                    knot.Y++;
+                }
+            }
+        
+
+            public void FillRows()
+            {
+                foreach(var row in Positions)
+                {
+                    for (int x = MaxY - row.Count; x>=0; x--)
+                    {
+                        row.Add(false);
+                    }
+                }
+            }
+
+            public string CountVists()
+            {
+                int visits = 0;
+                foreach (var row in Positions)
+                {
+                    visits += row.Count(r => r);
+                }
+                return visits.ToString();
+            }
+
+            public void MoveX(Knot lead, Knot follow)
+            {
+                follow.X = lead.X > follow.X ? follow.X + 1 : follow.X - 1;
+            }
+            public void MoveY(Knot lead, Knot follow)
+            {
+                follow.Y = lead.Y > follow.Y ? follow.Y + 1 : follow.Y - 1;
+            }
+
+        }
+
         public string Puzzle1(string[] input)
         {
-            throw new NotImplementedException();
+            var grid = new Grid(2);
+            foreach (var instruction in input)
+            {
+                List<string> instructionParts = instruction.Split(' ').ToList();
+                grid.Move(Convert.ToChar(instructionParts[0]), Convert.ToInt16(instructionParts[1]));
+            }
+
+            return grid.CountVists();
         }
 
         public string Puzzle2(string[] input)
         {
-            throw new NotImplementedException();
+            var grid = new Grid(10);
+            foreach (var instruction in input)
+            {
+                List<string> instructionParts = instruction.Split(' ').ToList();
+                grid.Move(Convert.ToChar(instructionParts[0]), Convert.ToInt16(instructionParts[1]));
+            }
+
+            return grid.CountVists();
         }
     }
 }
